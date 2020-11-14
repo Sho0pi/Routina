4284ab04faa872c22a39764e1bba84410083ad83
sho0pi <itayblokh@gmail.com>
2020-11-14 21:37:36 +0200

    set the action factories as mutable

--- app/src/main/java/com/shoopi/routina/actionsholders/BasicActionHolder.kt
+++ app/src/main/java/com/shoopi/routina/actionsholders/BasicActionHolder.kt
@@ -3,11 +3,7 @@ package com.shoopi.routina.actionsholders
 import com.shoopi.routina.actions.ActionFactory
 
 class BasicActionHolder() {
-    private var actionFactories: List<ActionFactory<*>> = ArrayList()
-
-    fun addActionFactory(actionFactory: ActionFactory<*>) {
-        actionFactories += actionFactory
-    }
+    private var actionFactories: MutableList<ActionFactory<*>> = ArrayList()
 
     fun execute(): Any? {
         var previousResult: Any? = null
@@ -17,5 +13,23 @@ class BasicActionHolder() {
         }
         return previousResult
     }
+
+    fun add(actionFactory: ActionFactory<*>): Boolean {
+        return actionFactories.add(actionFactory)
+    }
+
+    fun add(position: Int, actionFactory: ActionFactory<*>) {
+        return actionFactories.add(position, actionFactory)
+    }
+
+    fun remove(actionFactory: ActionFactory<*>): Boolean {
+        return actionFactories.remove(actionFactory)
+    }
+
+    fun removeAt(index: Int): ActionFactory<*> {
+        return actionFactories.removeAt(index)
+    }
+
+    fun size() = actionFactories.size
 
 }
